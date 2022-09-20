4d1af85ef99b19d3739568d410bed4c399241c63
Masakazu Kizawa <30505094+mskz930@users.noreply.github.com>
2022-09-20 12:00:36 +0900

    Update Service.jl

--- service/Service.jl
+++ service/Service.jl
@@ -12,7 +12,9 @@ using DataFrames
 abstract type AbstractService end
 abstract type LoginService <: AbstractService end
 
-function login(::Type{LoginService}, dto::LoginDto)::UserDto
+login(::Type{LoginService}, args...) = login(LoginService(), args...);
+
+function get_user(::LoginService, dto::LoginDto)::UserDto
     try
         conn = DB.get_conn()
         sql = "select id, name, email from users where id = ?"
@@ -32,11 +34,11 @@ function login(::Type{LoginService}, dto::LoginDto)::UserDto
     row = rows[1, :]
     user = UserDto(id=row.id, name=row.name, email=row.email)
     return user
-
 end
 
+authenticate(user::UserDto, password::AbstractString) = user.password == password
 
-function test(::Model.UserDto)
-end
+
+
 
 end # module
