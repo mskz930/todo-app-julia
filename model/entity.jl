b842b9b978a27f5c4329f8ced13cac0d4ed51e84
Masakazu Kizawa <30505094+mskz930@users.noreply.github.com>
2022-09-20 11:42:43 +0900

    Update entity.jl

--- model/entity.jl
+++ model/entity.jl
@@ -1,13 +1,24 @@
 using Dates: DateTime
+using Base: @kwdef
 
 abstract type AbstractEntity end
 
-
-struct Todo <: AbstractEntity
+# todo entity
+@kwdef struct Todo <: AbstractEntity
     id::Int
     user_id::Int
     category_id::Int
     task::String
+    created_at::DateTime
+    updated_at::DateTime
+end
+
+# user enitity
+@kwdef struct User <: AbstractEntity
+    id::Int
+    name::String
+    email::String
+    password::String
     created_at::DateTime
     updated_at::DateTime
 end
